b8d42963a13f5accf1b2c72d96757f3a158977ec
jmpl10 <jmpl@educa.madrid.org>
2025-03-12 13:29:42 +0100

    v4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-
     System.out.println("Hola!");
-
     System.out.println("Hola2!");
-
     System.out.println("Hola3!");
+    System.out.println("Hola4!");
   }
 }
